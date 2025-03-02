7a5ab76114e4172790d33fb7c945eb61f5dcff8e
Tvs Chaitanya <tvschaitanya@gmail.com>
2025-03-02 11:36:16 -0500

    added React using Vite

--- server/index.js
+++ server/index.js
@@ -4,11 +4,19 @@ import TodoRoute from './routes/todo.routes.js';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
 dotenv.config();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api/user", AuthRoute);
